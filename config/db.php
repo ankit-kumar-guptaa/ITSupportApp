fdc5889913efca8dff7bddc5b96eb3a3869551aa
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2025-03-22 11:42:57 +0530

    Update database configuration and add initial CSS styles for the project

--- config/db.php
+++ config/db.php
@@ -1,12 +1,15 @@
 <?php
-// $host = 'localhost';
-// $dbname = 'itsupport_db';
-// $username = 'root';
-// $password = '';
 $host = 'localhost';
-$dbname = 'u634223065_itsupport_db';
-$username = 'u634223065_itsupport_db';
-$password = 'Ankit@1925@';
+$dbname = 'itsupport_db';
+$username = 'root';
+$password = '';
+
+// For online server
+
+// $host = 'localhost';
+// $dbname = 'u634223065_itsupport_db';
+// $username = 'u634223065_itsupport_db';
+// $password = 'Ankit@1925@';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
