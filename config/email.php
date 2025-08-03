8eb3c9630b85666d6f909a5501effdc4a9a7e502
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2025-08-03 13:47:07 +0530

    smtp fix

--- config/email.php
+++ config/email.php
@@ -11,13 +11,13 @@ function sendEmail($to, $subject, $body) {
         $mail->isSMTP();
         $mail->Host = 'smtp.hostinger.com'; // Gmail SMTP server
         $mail->SMTPAuth = true;
-        $mail->Username = 'rajiv@greencarcarpool.com'; // Apna Gmail daal
-        $mail->Password = 'Rajiv@111@'; // Gmail App Password (not regular password)
+        $mail->Username = 'no-reply@itsahayata.com'; // Apna Gmail daal
+        $mail->Password = 'Support@1925'; // Gmail App Password (not regular password)
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         // Recipients
-        $mail->setFrom('rajiv@greencarcarpool.com', 'IT Support Hub');
+        $mail->setFrom('no-reply@itsahayata.com', 'IT Support Hub');
         $mail->addAddress($to);
 
         // Content
